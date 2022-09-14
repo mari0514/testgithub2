41cfd008cfb51eee2e3204ab1f8e95e2b574f19d
mari0514 <111376774+mari0514@users.noreply.github.com>
2022-09-14 10:14:42 -0700

    JS practice

--- part1/text.js
+++ part1/text.js
@@ -16,6 +16,7 @@ function sayHi(firstName, lastName, age) {
 }
 
 console.log(sayHi("Mari", "Miyazaki", 24));
+console.log("=========================================");
 
 
 // array
@@ -34,5 +35,12 @@ arr.push("Yep", "Nope", "idk");
 console.log(arr);
 
 console.log(numberArr.sort());
+
+console.log("=========================================");
+
+// sort
+let numbers = [5, 2, 14, 27, 56, 32, 4, 9, 11];
+numbers.sort((a, b) => a - b);
+console.log("sort " + numbers);
 
 
