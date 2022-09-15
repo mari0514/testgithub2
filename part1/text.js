4ffbc5606f9904eeeb2d0e2681a57bca7660cd48
mari0514 <111376774+mari0514@users.noreply.github.com>
2022-09-15 11:19:58 -0700

    JS practice

--- part1/text.js
+++ part1/text.js
@@ -1,4 +1,5 @@
 // variables
+console.log("variables");
 var car = {
   model: "Mustang",
   color: "red",
@@ -11,6 +12,7 @@ console.log(car.year);
 
 
 // function
+console.log("function");
 function sayHi(firstName, lastName, age) {
   return `Hi, ${firstName} ${lastName} (${age})`;
 }
@@ -20,6 +22,7 @@ console.log("=========================================");
 
 
 // array
+console.log("array");
 let arr = [5, 14, "Mari", "M"];
 let numberArr = [50, 24, 2, 78, 30, 5, 14, 124];
 console.log(arr[2]);
@@ -43,4 +46,60 @@ let numbers = [5, 2, 14, 27, 56, 32, 4, 9, 11];
 numbers.sort((a, b) => a - b);
 console.log("sort " + numbers);
 
+console.log("=========================================");
 
+//function scope & block scope
+console.log("scope");
+function start() {
+  var varCount = 0;
+  let letCount = 0;
+
+  console.log(varCount, letCount);
+
+  {
+    var count1 = 0;
+    let count2 = 0;
+
+    console.log("var variable ", count1, " let variable ", count2);
+  }
+  console.log("var varialbe ", count1);
+  // console.log("let varialbe ", count2); error occured
+
+}
+start();
+
+console.log("=========================================");
+
+
+// for structure
+console.log("for structure");
+// var functions = [];
+
+// for(var i = 0; i < 3; i++) {
+//   functions[i] = function() {
+//     console.log("Chapter " + (i + 1));
+//   };
+// }
+
+// for(var j = 0; j < functions.length; j++) {
+//   functions[j]();
+// }
+
+console.log("=========================================");
+
+
+// あ
+function START() {
+  var name1 = "Joe";
+
+  function displayTheName() {
+    console.log(name1);
+  }
+  displayTheName();
+}
+
+START();
+
+console.log("=========================================");
+
+console.log("a");
