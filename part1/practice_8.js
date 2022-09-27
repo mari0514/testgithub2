cd7c56336ae41a475a1dc346406f5ccef6e4e787
mari0514 <111376774+mari0514@users.noreply.github.com>
2022-09-27 09:04:53 -0700

    commit

---
+++ part1/practice_8.js
@@ -0,0 +1,9 @@
+
+// Sep. 27
+let arr = [89, 53, 6, 2, 4, 1, 8, 5, 15, 10, 12, 35, 60, 0, 24, 65, 3, 14, 74];
+let strArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"];
+let target = 60;
+let targetStr = "g";
+
+console.log(arr.includes(target));
+console.log(strArr.includes(targetStr));
