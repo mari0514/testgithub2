9cbbd4670079cda0561c24376ce086fe729f390e
mari0514 <111376774+mari0514@users.noreply.github.com>
2022-09-28 12:15:14 -0700

    commit

--- part2/sorting_algorithm.js
+++ part2/sorting_algorithm.js
@@ -97,3 +97,26 @@ function selectionSort2(arr) {
 }
 
 console.log(selectionSort2(array));
+
+
+
+// Insertion sort algorithm
+
+console.log("Insertion sort");
+
+function insertionSort(arr)  {  
+    let i, key, j;  
+    for (i = 1; i < arr.length; i++) {  
+        key = arr[i];  
+        j = i - 1;
+        while (j >= 0 && arr[j] > key) {  
+            arr[j + 1] = arr[j];  
+            j = j - 1;  
+        }  
+        arr[j + 1] = key;  
+    }  
+    return arr;
+}  
+
+console.log(insertionSort(array));
+
