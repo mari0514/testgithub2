6bb2bec35f3666dada418f7df3ac45c230cbb076
mari0514 <111376774+mari0514@users.noreply.github.com>
2022-10-01 14:55:48 -0700

    commit

---
+++ part2/sorting_practice.js
@@ -0,0 +1,7 @@
+/*
+Exercise 1:
+Apply the correct Array method for sorting the vegetables array alphabetically.
+*/
+
+let vegetables = ["Garlic", "Carrot", "Broccoli", "Pumpkin"];
+console.log(vegetables.sort());
